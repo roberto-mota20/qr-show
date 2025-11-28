4400966ae27930322e8e9048ed6501fc1af32de8
Roberto Mota <roberto.cmota20@gmail.com>
2025-11-28 00:19:19 +0000

    Implementação de sistema de analise de trafego

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
-import '../styles/globals.css'
-import Head from 'next/head'
+import '../styles/globals.css';
+import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 // Componente principal que envolve todas as páginas.
-// É aqui que importamos o CSS global.
+// É aqui que importamos o CSS global e agora o Analytics.
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      
+      {/* Componente da página atual */}
       <Component {...pageProps} />
+      
+      {/* Componente do Vercel Analytics (invisível na tela, mas coleta os dados) */}
+      <Analytics />
     </>
-  )
+  );
 }
